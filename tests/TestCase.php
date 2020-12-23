d683412e4428b260484c331844887fa3ee47a581
nagi1 <nagi1@users.noreply.github.com>
2020-12-23 09:12:47 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Nagi1\MyFatoorah\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Nagi1\MyFatoorah\MyFatoorahServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
